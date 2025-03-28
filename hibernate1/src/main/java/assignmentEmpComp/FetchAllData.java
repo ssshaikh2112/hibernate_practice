cbc91ffe1fe08108e55f4c17834e1a5c77a918da
Sahil Shaikh <105914225+ssshaikh2112@users.noreply.github.com>
2025-03-28 15:16:27 +0530

    Update FetchAllData.java

--- hibernate1/src/main/java/assignmentEmpComp/FetchAllData.java
+++ hibernate1/src/main/java/assignmentEmpComp/FetchAllData.java
@@ -1,7 +1,6 @@
 package assignmentEmpComp;
 
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
